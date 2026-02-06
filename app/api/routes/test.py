aef533dfef48eb91864a72d14e0b2700c875eedb
Razvan Sauciuc <razvansau9@gmail.com>
2026-02-06 17:31:00 +0200

    fix: removed venv from the repo

---
+++ app/api/routes/test.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Request
+
+from app.api.models.user import User
+from app.api.routes.auth.auth import get_current_active_user
+from ..rate_limiter import limiter, RateLimitConfig
+
+router = APIRouter(prefix="/test", tags=["test"])
+
+@router.get("/")
+@limiter.limit(RateLimitConfig.READ)
+async def get(request: Request):
+    return {
+        "detail": "Hello world"
+    }
+
+@router.get("/protected")
+@limiter.limit(RateLimitConfig.READ)
+async def protected(
+    request: Request,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return {
+        "detail": "Hello, " + current_user.full_name + " (@" + current_user.username + ")", # type: ignore
+    }
