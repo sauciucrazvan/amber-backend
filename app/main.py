9e39b6e79cac7909484fce6523396bc10da23c2d
Razvan S. <razvansau9@gmail.com>
2026-02-18 21:31:07 +0200

    chore: implemented websockets

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ from app.api.rate_limiter import setup_rate_limiting
 from app import config as conf
 from app.api.routes.account import account
 from app.api.routes.auth import auth
-# from app.api.routes import ws
 from app.database import session
+from app.ws import connection_manager
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,7 +34,7 @@ def create_app(*, init_db: bool = True, enable_rate_limiting: bool = True) -> Fa
     api_router = APIRouter(prefix="/api")
     api_router.include_router(auth.router)
     api_router.include_router(account.router)
-    # api_router.include_router(ws.router)
+    api_router.include_router(connection_manager.router)
     application.include_router(api_router)
 
     application.add_middleware(
