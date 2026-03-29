d1153736496cb6ff1146f266325f11bb1bb8b7d0
Razvan <razvansau9@gmail.com>
2026-03-29 16:33:22 +0300

    fix: invalid alembic migration

--- alembic/versions/f5a3c2b1e8d0_add_call_audit_logs_and_metrics_tables.py
+++ alembic/versions/f5a3c2b1e8d0_add_call_audit_logs_and_metrics_tables.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "f5a3c2b1e8d0"
-down_revision: Union[str, Sequence[str], None] = "f43148259210"
+down_revision: Union[str, Sequence[str], None] = ("f43148259210", "7e4a9f9a1c0d")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
